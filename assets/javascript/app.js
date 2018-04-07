f4451b9b95090059d2a12e5057446f31720b3aee
Willie Smalls <williesmalls@Willies-MacBook-Air.local>
2018-04-07 12:20:30 -0400

    app.js updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,8 @@
 
-    var walmart_query = 'ipod';
+    
+    var productSearch = 'MacBook Air'
+    
+    var walmart_query = productSearch;
     var walmart_apiKey = 'wymapcqzkbzwruabx9t3cefx';
 
 
@@ -12,11 +15,16 @@
         method: "POST"
       }).then(function(walmart_response) {
         // Get reference to existing tbody element, create a new table row element
-          console.log("walmart", walmart_response);
+          //console.log("walmart", walmart_response);
           console.log("walmart items", walmart_response.items);
+          const{items} = walmart_response; 
+         
+         for (i = 0; i < 5; i++) {
+            console.log("item " + i +":  "+ items[i].name, "sales price:  " + items[i].salePrice, "medium image:   " + items[i].mediumImage)
+        }
       });
 
-      var bestbuy_query = 'ipod';
+      var bestbuy_query = productSearch;
       var bestbuy_apiKey = 'N45Lkw1tBElVvgFZZmAYoPaw';
       var bestbuy_queryURL = 'https://api.bestbuy.com/v1/products((search=' + bestbuy_query +'))?apiKey=' + bestbuy_apiKey + '&format=json';
   
@@ -26,7 +34,13 @@
       }).then(function(bestbuy_response) {
         // Get reference to existing tbody element, create a new table row element
           console.log("best buy", bestbuy_response);
-          console.log("best buy products", bestbuy_response.products);
+          //console.log("best buy products", bestbuy_response.products);
+
+          const{products} = bestbuy_response; 
+
+          for (i = 0; i < 5; i++) {
+            console.log("item " + i +":  "+ products[i].name, "sales price:  " + products[i].salePrice, "medium image:   " + products[i].image)
+        }
       });
 
 
