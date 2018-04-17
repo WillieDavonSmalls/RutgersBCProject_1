fc8c5273ef7e14a4bbfffca53539b7ab3ae926ae
Willie Smalls <williesmalls@Willies-MacBook-Air.local>
2018-04-17 17:42:57 -0400

    added BestBuy to Categories

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -247,30 +247,136 @@ for(var i = 0; i < catBinsArray.length ; i++){
 }
 
 // //loop through array of keys /// this is moving slowly. 
+var singleProduct;
 var prodArrayFromCatBins = []; 
 for(var i = 0; i < catBinsArray.length; i++){
-  var prodArrayFromCatBins = [];
   var binKey = catBinsArray[i];
   prodArrayFromCatBins = catBinsObj2[binKey];
   
   if(prodArrayFromCatBins != ''){
-    //console.log(binKey, prodArrayFromCatBins);
-    var tag = "$('" + '#tbl-'+ binKey + "')"
-    var filler = 
-    "'" + '<tr>' +
-    //product
-    '<td>' + 'Product' + '</td>'+
-    //Product Image
-    '<td><img class="result-thumbnail" src='+ '"http://via.placeholder.com/140x100"' + ' alt = "product" width="140"> </td>' +
-    //Vendor
-    '<td><img class="vendor-logo" src="assets/images/best-buy-logo-transparent.png" alt="bestbuy"></td>'+ 
-    //Price
-    '<td>$4.77</td>'+
-    '</tr>' + "'"
+    console.log(binKey, prodArrayFromCatBins);
+    for(var j = 0; j < prodArrayFromCatBins.length; j++){
+      singleProduct = prodArrayFromCatBins[j];
 
-    $('#tbl-' + binKey).append("'" + filler + "'");
+      searchBestBuyGroup(binKey, singleProduct);
+      searchWalmartGroup(binKey, singleProduct);
+
+
+    }
+
 
   }
   
 }
 
+//////////////////////////////////////////////////////////// Best Buy Fncn Group /////////////////////////////////////////////////////////
+// //Best Buy function 
+function searchBestBuyGroup(binKey, singleProduct){
+
+  //BestBuy API variables
+  var bestbuy_query = singleProduct;
+  var bestbuy_apiKey = 'N45Lkw1tBElVvgFZZmAYoPaw';
+  var bestbuy_queryURL = 'https://api.bestbuy.com/v1/products((search=' + bestbuy_query + '))?apiKey=' + bestbuy_apiKey + '&format=json';
+  var bestbuy_logo = '<img class="vendor-logo" src="assets/images/best-buy-logo-transparent.png" alt="bestbuy">'
+
+  //BestBuy API Call
+  $.ajax({
+    url: bestbuy_queryURL,
+    method: 'GET'
+  }).then(function(bestbuy_response) {
+    // Get reference to existing tbody element, create a new table row element
+      //console.log("best buy", bestbuy_response);
+      //console.log("best buy products", bestbuy_response.products);
+
+      //constructor for products
+      const{products} = bestbuy_response; 
+      
+      //for loop through Best Buy API
+      for (i = 0; i < 5; i++) {
+        //console.log("item " + i +":  "+ products[i].name, "sales price:  " + products[i].salePrice, "medium image:   " + products[i].image)
+        
+        var bestBuyImage = products[i].image;
+
+        if(bestBuyImage != null){
+          bestBuyImage = bestBuyImage;
+        }
+        else{
+          bestBuyImage = missingImage;
+        }
+        var tag = "$('" + '#tbl-'+ binKey + "')"
+        var filler = 
+        "'" + '<tr>' +
+        //product
+        '<td>' + products[i].name  + '</td>'+
+        //Product Image
+        '<td><img class="result-thumbnail" src="' + bestBuyImage + '" alt = "product" width="140"></td>' +
+        //Vendor
+        '<td>' + bestbuy_logo + '</td>' +
+        //Price
+        '<td>$' + products[i].salePrice + '</td>'
+        +'</tr>';
+  
+        $('#tbl-' + binKey).append("'" + filler + "'");
+        
+
+      }
+  });
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  //walmart function 
+  function searchWalmartGroup(binKey, singleProduct){
+
+    //variables for Wal-mart function 
+    var walmart_query = productSearch;
+    var walmart_apiKey = 'wymapcqzkbzwruabx9t3cefx';
+    var walmart_logo = '<td><img class="vendor-logo" src="assets/images/walmart-logo-transparent.png" alt="walmart"></td>'
+
+    //Wal-mart API function 
+    $.ajax({
+        url: "https://mighty-river-19291.herokuapp.com/cors",
+        data: {
+            url:'http://api.walmartlabs.com/v1/search?apiKey=' + walmart_apiKey + '&query=' + walmart_query,
+            key:"8b5dcaf7cdfb9c46221d492eec6560c571d6ec218b2485c54075ab7840fa77f9"
+        },
+        method: "POST"
+      }).then(function(walmart_response) {
+        // Get reference to existing tbody element, create a new table row element
+          //console.log("walmart", walmart_response);
+          //console.log("walmart items", walmart_response.items);
+          
+          //constructor for items 
+          const{items} = walmart_response; 
+         
+         //for loop to loop through wal-mart products 
+         for (i = 0; i < 5; i++) {
+            //console.log("item " + i +":  "+ items[i].name, "sales price:  " + items[i].salePrice, "medium image:   " + items[i].mediumImage)
+
+            var walmartImage = items[i].mediumImage;
+
+            if(walmartImage != null){
+              walmartImage = walmartImage;
+            }
+            else{
+              walmartImage = missingImage;
+            }
+
+            var tag = "$('" + '#tbl-'+ binKey + "')"
+            var filler = 
+            "'" + '<tr>' +
+            //product
+            '<td>' + items[i].name  + '</td>'+
+            //Product Image
+            '<td><img class="result-thumbnail" src="' + walmartImage  + '" alt = "product" width="140"></td>' +
+            //Vendor
+            '<td>' + walmart_logo + '</td>' +
+            //Price
+            '<td>$' + items[i].salePrice+ '</td>'
+            +'</tr>';
+      
+            $('#tbl-' + binKey).append("'" + filler + "'");
+
+
+        }
+      });
+    }
