709446f8177ec33a5a4a68a989d24e1e611ce56f
Willie Smalls <williesmalls@Willies-MacBook-Air.local>
2018-04-18 07:50:52 -0400

    fixed Wal-mart table

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -369,7 +369,7 @@ function searchBestBuyGroup(binKey, singleProduct){
             //Product Image
             '<td><img class="result-thumbnail" src="' + walmartImage  + '" alt = "product" width="140"></td>' +
             //Vendor
-            '<td>' + walmart_logo + '</td>' +
+            + walmart_logo +
             //Price
             '<td>$' + items[i].salePrice+ '</td>'
             +'</tr>';
